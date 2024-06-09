3fa53dac6b8827e9a5f5a1d69c3cabe7fd7a0117
muzaffarxoja <muzsamgio@gmail.com>
2024-06-09 14:28:24 +0500

    a

---
+++ lib/models/character.dart
@@ -0,0 +1,91 @@
+
+import 'package:json_annotation/json_annotation.dart';
+import 'package:json_serializable/json_serializable.dart';
+
+part 'character.g.dart';
+
+@JsonSerializable()
+
+class Character {
+  Character({
+    required this.id,
+    required this.name,
+    required this.status,
+    required this.species,
+    required this.type,
+    required this.gender,
+
+    required this.image,
+    required this.episode,
+    required this.url,
+    required this.created,
+    //required this.origin,
+    //required this.location,
+  });
+
+  final int id;
+  final String name;
+  final String status;
+  final String species;
+  final String type;
+  final String gender;
+  final String image;
+  final List<String> episode;
+  final String url;
+  final DateTime created;
+
+  factory Character.fromJson(Map<String, dynamic> json) =>
+      _$CharacterFromJson(json);
+
+  /// Connect the generated [_$PersonToJson] function to the `toJson` method.
+  Map<String, dynamic> toJson() => _$CharacterToJson(this);
+
+}
+
+//final CharacterLocation origin;
+  // final CharacterLocation location;
+
+  // factory Character.fromJson(Map<String, dynamic> json) => Character(
+  //   id: json["id"],
+  //   name: json["name"],
+  //   status: json["status"],
+  //   species: json["species"],
+  //   type: json["type"],
+  //   gender: json["gender"],
+  //   origin: CharacterLocation.fromJson(json["origin"]),
+  //   location: CharacterLocation.fromJson(json["location"]),
+  //   image: json["image"],
+  //   episode: List<String>.from(json["episode"].map((x) => x)),
+  //   url: json["url"],
+  //   created: DateTime.parse(json["created"]),
+  // );
+//
+//   Map<String, dynamic> toJson() => {
+//     "id": id,
+//     "name": name,
+//     "status": status,
+//     "species": species,
+//     "type": type,
+//     "gender": gender,
+//     "origin": origin.toJson(),
+//     "location": location.toJson(),
+//     "image": image,
+//     "episode": List<dynamic>.from(episode.map((x) => x)),
+//     "url": url,
+//     "created": created.toIso8601String(),
+//   };
+
+
+
+
+
+
+// class CharacterLocation {
+//   CharacterLocation({
+//     required this.name,
+//     required this.url,
+//   });
+//
+//   String name;
+//   String url;
+// }
